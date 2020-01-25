9b252dc9b1b7592ec5f2c810eb5407696ba0e4c3
fenix15100 <francisco.camacho15@gmail.com>
2020-01-25 13:52:02 +0100

    create landing grid

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import {Grid, Cell} from 'react-mdl'
 
 const Landing = () => {
     return (
-        <div>
-            <h1>Lading Page</h1>
+        <div style={{width: '100%',margin: 'auto'}}>
+            <Grid className="landing-grid">
+                <Cell col={12}></Cell>
+            </Grid>
+
         </div>
         
     );
