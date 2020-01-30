b9041c5c515c313fa5c901c51af155de64eaafd4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-01-30 10:30:54 +0000

    Deploy fenix15100/react-portfolio to github.com/fenix15100/react-portfolio.git:gh-pages

---
+++ precache-manifest.8d41136bf0f1369997b0583061e484c8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc3c7cb7729a2b56a7f5dc2989e8ed6",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "5ac57e23201ad8fb3f9a",
+    "url": "/react-portfolio/static/css/2.a6bc1c31.chunk.css"
+  },
+  {
+    "revision": "45b16ec21d6db2959ca4",
+    "url": "/react-portfolio/static/css/main.165ee49b.chunk.css"
+  },
+  {
+    "revision": "5ac57e23201ad8fb3f9a",
+    "url": "/react-portfolio/static/js/2.e7f233ee.chunk.js"
+  },
+  {
+    "revision": "f111f0e43b20013304c8bd0659a06b92",
+    "url": "/react-portfolio/static/js/2.e7f233ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45b16ec21d6db2959ca4",
+    "url": "/react-portfolio/static/js/main.6f29e842.chunk.js"
+  },
+  {
+    "revision": "9f289a7f9c113993272b",
+    "url": "/react-portfolio/static/js/runtime-main.f68edf73.js"
+  },
+  {
+    "revision": "bb0fd3e12502e78b6c2fe9ec11363bcc",
+    "url": "/react-portfolio/static/media/avatar.bb0fd3e1.png"
+  }
+]);
